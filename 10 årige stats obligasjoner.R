a39093e3d5e1745e19a6e05bf65155e176b23308
Johnrejor <42776281+Johnrejor@users.noreply.github.com>
2018-12-09 14:56:52 +0100

    Add files via upload

--- 10 årige stats obligasjoner.R
+++ 10 årige stats obligasjoner.R
@@ -9,59 +9,53 @@ library(zoo)
 install.packages("scales")
 library(scales)
 
-
+Stats_obligasjoner_long_term <- read.csv("https://raw.githubusercontent.com/Johnrejor/lecture_3.0/master/Stats%20obligasjoner%20long%20term.csv")
 Stats_obligasjoner_long_term <- as.data.frame(sapply(Stats_obligasjoner_long_term, function(x) gsub("\"", "", x)))
 
 Stats_obligasjoner_long_term <- splitstackshape::cSplit(Stats_obligasjoner_long_term, names(Stats_obligasjoner_long_term)) 
 
-names(Stats_obligasjoner_long_term)[1] <- paste("Location") 
-names(Stats_obligasjoner_long_term)[2] <- paste("Indicator")
-names(Stats_obligasjoner_long_term)[3] <- paste("Subject")
-names(Stats_obligasjoner_long_term)[4] <- paste("Measure")
-names(Stats_obligasjoner_long_term)[5] <- paste("Freq")
-names(Stats_obligasjoner_long_term)[6] <- paste("Time")                                                                     
-names(Stats_obligasjoner_long_term)[7] <- paste("Value")
-names(Stats_obligasjoner_long_term)[8] <- paste("flag codes")
+#Removing uninteresting columns
+Stats_obligasjoner_long_term<- Stats_obligasjoner_long_term[, -c(2:5)]
 
-Stats_obligasjoner_long_term$Indicator = NULL
-Stats_obligasjoner_long_term$Freq = NULL 
-Stats_obligasjoner_long_term$Subject = NULL 
-Stats_obligasjoner_long_term$Measure = NULL 
-Stats_obligasjoner_long_term$Measure = NULL 
+#Changing colnames
+colnames(Stats_obligasjoner_long_term) <- c("Location", "Time", "Value")
+
+
+
 Stats_obligasjoner_long_term$Location <- as.character(Stats_obligasjoner_long_term$Location)  
 Stats_obligasjoner_long_term$Time <- as.Date(as.yearmon(Stats_obligasjoner_long_term$Time))
 
 Irland2 <- filter(Stats_obligasjoner_long_term, Location == "IRL")
 
-ggplot(data = Irland) + geom_point(mapping = aes(x = Time, y = Value)) + scale_x_date(date_breaks = "2 year", labels=date_format("%Y"))
+ggplot(data = Irland2) + geom_line(mapping = aes(x = Time, y = Value)) + scale_x_date(date_breaks = "2 year", labels=date_format("%Y"))
 Greece2 <- filter(Stats_obligasjoner_long_term, Location == "GRC")
 
-ggplot(data = Greece) + geom_line(mapping = aes(x = Time, y = Value)) + scale_x_date(date_breaks = "2 year", labels=date_format("%Y"))
+ggplot(data = Greece2) + geom_line(mapping = aes(x = Time, y = Value)) + scale_x_date(date_breaks = "2 year", labels=date_format("%Y"))
 
 Norway2 <- filter(Stats_obligasjoner_long_term, Location == "NOR")
 
-ggplot(data = Norway) + geom_line(mapping = aes(x = Time, y = Value)) + scale_x_date(date_breaks = "2 year", labels=date_format("%Y"))
+ggplot(data = Norway2) + geom_line(mapping = aes(x = Time, y = Value)) + scale_x_date(date_breaks = "2 year", labels=date_format("%Y"))
 
 Spain2 <- filter(Stats_obligasjoner_long_term, Location == "ESP")
 
-ggplot(data = Spain) + geom_line(mapping = aes(x = Time, y = Value)) + scale_x_date(date_breaks = "2 year", labels=date_format("%Y"))
+ggplot(data = Spain2) + geom_line(mapping = aes(x = Time, y = Value)) + scale_x_date(date_breaks = "2 year", labels=date_format("%Y"))
 
 Italy2 <- filter(Stats_obligasjoner_long_term, Location == "ITA")
 
-ggplot(data = Italy) + geom_line(mapping = aes(x = Time, y = Value)) + scale_x_date(date_breaks = "2 year", labels=date_format("%Y"))
+ggplot(data = Italy2) + geom_line(mapping = aes(x = Time, y = Value)) + scale_x_date(date_breaks = "2 year", labels=date_format("%Y"))
 
 Canada2 <- filter(Stats_obligasjoner_long_term, Location == "CAN")
 
-ggplot(data = Canada) + geom_line(mapping = aes(x = Time, y = Value)) + scale_x_date(date_breaks = "2 year", labels=date_format("%Y"))
+ggplot(data = Canada2) + geom_line(mapping = aes(x = Time, y = Value)) + scale_x_date(date_breaks = "2 year", labels=date_format("%Y"))
 
 USA2 <- filter(Stats_obligasjoner_long_term, Location == "USA")
 
-ggplot(data = USA) + geom_line(mapping = aes(x = Time, y = Value)) + scale_x_date(date_breaks = "2 year", labels=date_format("%Y"))
+ggplot(data = USA2) + geom_line(mapping = aes(x = Time, y = Value)) + scale_x_date(date_breaks = "2 year", labels=date_format("%Y"))
 
 Island2 <- filter(Stats_obligasjoner_long_term, Location == "ISL")
 
 
-ggplot(data = Island) + geom_line(mapping = aes(x = Time, y = Value)) + scale_x_date(date_breaks = "2 year", labels=date_format("%Y"))
+ggplot(data = Island2) + geom_line(mapping = aes(x = Time, y = Value)) + scale_x_date(date_breaks = "2 year", labels=date_format("%Y"))
 
 
 
--- USA privatgjeld.R
+++ USA privatgjeld.R
@@ -3,77 +3,45 @@ library(tidyverse)
 library(mosaic) 
 library(splitstackshape)
 library(dplyr)
-hushold_gjeld <- read_csv("https://raw.githubusercontent.com/Derraz91/Data-prosjekt/master/Unemployment2000-2018.csv")
+hushold_gjeld <- read_csv("https://raw.githubusercontent.com/Johnrejor/lecture_3.0/master/hushold%20gjeld.csv")
 
 #Removing quotation marks, does not remove it from colnames
-Unemp_OECD <- as.data.frame(sapply(Unemp_OECD, function(x) gsub("\"", "", x)))
+hushold_gjeld <- as.data.frame(sapply(hushold_gjeld, function(x) gsub("\"", "", x)))
 
 #Split up colnames
-Unemp_OECD <- splitstackshape::cSplit(Unemp_OECD, names(Unemp_OECD))
+hushold_gjeld <- splitstackshape::cSplit(hushold_gjeld, names(hushold_gjeld))
 
 
 #Removing uninteresting columns
-Unemp_OECD <- Unemp_OECD[, -c(2:5, 8)]
+hushold_gjeld <- hushold_gjeld[, -c(2:5)]
 
 #Changing colnames
-colnames(Unemp_OECD) <- c("Location", "Time", "Value")
+colnames(hushold_gjeld) <- c("Location", "Time", "Value")
 
 #Setting column Location as.character
-Unemp_OECD$Location <- as.character(Unemp_OECD$Location)
-
-#Changing Qartile to month and day
-Unemp_OECD$Time <- as.Date(as.yearqtr(Unemp_OECD$Time, format = "%Y-Q%q"))
-
-
-
-hushold_gjeld <- read_csv("R/hushold gjeld.csv")
-
-hushold_gjeld <- as.data.frame(sapply(hushold_gjeld, function(x) gsub("\"", "", x)))
-
-hushold_gjeld <- splitstackshape::cSplit(hushold_gjeld, names(hushold_gjeld)) 
-
-names(hushold_gjeld)[1] <- paste("Location") 
-names(hushold_gjeld)[2] <- paste("Indicator")
-names(hushold_gjeld)[3] <- paste("Subject")
-names(hushold_gjeld)[4] <- paste("Measure")
-names(hushold_gjeld)[5] <- paste("Freq")
-names(hushold_gjeld)[6] <- paste("Time")                                                                     
-names(hushold_gjeld)[7] <- paste("Value")
-names(hushold_gjeld)[8] <- paste("flag codes")
-
-hushold_gjeld$Indicator = NULL
-hushold_gjeld$Freq = NULL 
-hushold_gjeld$Subject = NULL 
-hushold_gjeld$Measure = NULL 
-hushold_gjeld$Measure = NULL 
 hushold_gjeld$Location <- as.character(hushold_gjeld$Location)
-australia <- filter(hushold_gjeld, Location == "AUS")
-
-
-ggplot(data = australia) + geom_point(mapping = aes(x = Time, y = Value))
 
 
 
-GDP_percap1 <- read_csv("R/BNP.csv")
+
+
+
+
+
+GDP_percap1 <- read_csv("https://raw.githubusercontent.com/Johnrejor/lecture_3.0/master/GDP%20per%20capita%20from%20OECD.csv")
 
 GDP_percap1 <- as.data.frame(sapply(GDP_percap1, function(x) gsub("\"", "", x)))
 
 GDP_percap1 <- splitstackshape::cSplit(GDP_percap1, names(GDP_percap1)) 
 
-names(GDP_percap1)[1] <- paste("Location") 
-names(GDP_percap1)[2] <- paste("Indicator")
-names(GDP_percap1)[3] <- paste("Subject")
-names(GDP_percap1)[4] <- paste("Measure")
-names(GDP_percap1)[5] <- paste("Freq")
-names(GDP_percap1)[6] <- paste("Time")                                                                     
-names(GDP_percap1)[7] <- paste("Value")
-names(GDP_percap1)[8] <- paste("fC")
+#Removing uninteresting columns
+GDP_percap1<- GDP_percap1[, -c(2:5, 8)]
 
-GDP_percap1$Indicator = NULL
-GDP_percap1$Freq = NULL 
-GDP_percap1$Subject = NULL 
-GDP_percap1$Measure = NULL 
-GDP_percap1$fC = NULL 
+#Changing colnames
+colnames(GDP_percap1) <- c("Location", "Time", "Value")
+
+#Setting column Location as.character
+GDP_percap1$Location <- as.character(GDP_percap1$Location)
 
 merged2$Time <- as.Date(as.yearmon(Stats_obligasjoner_long_term$Time))
 
