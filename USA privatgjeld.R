af82fb2d7c35c5dbc852454867c96c73add833fe
Johnrejor <42776281+Johnrejor@users.noreply.github.com>
2018-12-09 14:03:09 +0100

    Add files via upload

--- USA privatgjeld.R
+++ USA privatgjeld.R
@@ -1,8 +1,30 @@
 library(readr)
 library(tidyverse)
-library(mosaic)
+library(mosaic) 
 library(splitstackshape)
 library(dplyr)
+hushold_gjeld <- read_csv("https://raw.githubusercontent.com/Derraz91/Data-prosjekt/master/Unemployment2000-2018.csv")
+
+#Removing quotation marks, does not remove it from colnames
+Unemp_OECD <- as.data.frame(sapply(Unemp_OECD, function(x) gsub("\"", "", x)))
+
+#Split up colnames
+Unemp_OECD <- splitstackshape::cSplit(Unemp_OECD, names(Unemp_OECD))
+
+
+#Removing uninteresting columns
+Unemp_OECD <- Unemp_OECD[, -c(2:5, 8)]
+
+#Changing colnames
+colnames(Unemp_OECD) <- c("Location", "Time", "Value")
+
+#Setting column Location as.character
+Unemp_OECD$Location <- as.character(Unemp_OECD$Location)
+
+#Changing Qartile to month and day
+Unemp_OECD$Time <- as.Date(as.yearqtr(Unemp_OECD$Time, format = "%Y-Q%q"))
+
+
 
 hushold_gjeld <- read_csv("R/hushold gjeld.csv")
 
@@ -53,23 +75,47 @@ GDP_percap1$Subject = NULL
 GDP_percap1$Measure = NULL 
 GDP_percap1$fC = NULL 
 
+merged2$Time <- as.Date(as.yearmon(Stats_obligasjoner_long_term$Time))
+
+
 merged2 <- left_join(hushold_gjeld, GDP_percap1, by=c("Location", "Time"))
 
-norway <- filter(hushold_gjeld, Location == "NOR")
+norway <- filter(merged2, Location == "NOR")
 
-ggplot(data = norway) + geom_point(mapping = aes(x = Time, y = Value))
+ggplot(data = norway) + geom_line(mapping = aes(x = Time, y = Value.x)) 
+ggplot(data = norway) + geom_line(mapping = aes(x = Time, y = Value.y))
 
-Estonia <-  filter(hushold_gjeld, Location == "EST")
+Irland <-  filter(merged2, Location == "IRL")
 
-ggplot(data = Estonia) + geom_point(mapping = aes(x = Time, y = Value))
+ggplot(data = Irland) + geom_line(mapping = aes(x = Time, y = Value.x))
+ggplot(data = Irland) + geom_line(mapping = aes(x = Time, y = Value.y))
 
 USA <- filter(merged2, Location == "USA")
 
-ggplot(data = USA) + geom_point(mapping = aes(x = Time, y = Value.x))
+ggplot(data = USA) + geom_line(mapping = aes(x = Time, y = Value.x))
 
 ggplot(data = USA) + geom_point(mapping = aes(x = Time, y = Value.y))
 
 Greece <- filter(merged2, Location == "GRC")
 
-ggplot(data = Greece) + geom_point(mapping = aes(x = Time, y = Value.x))
-ggplot(data = Greece) + geom_point(mapping = aes(x = Time, y = Value.x))
+ggplot(data = Greece) + geom_line(mapping = aes(x = Time, y = Value.x))
+ggplot(data = Greece) + geom_line(mapping = aes(x = Time, y = Value.y))
+
+Australia <- filter(merged2, Location == "AUS")
+
+ggplot(data = Australia) + geom_line(mapping = aes(x = Time, y = Value.x))
+
+ggplot(data = Australia) + geom_line(mapping = aes(x = Time, y = Value.y))
+Spain <- filter(merged2, Location == "ESP")
+ggplot(data = Spain) + geom_line(mapping = aes(x = Time, y = Value.x))
+ggplot(data = Australia) + geom_line(mapping = aes(x = Time, y = Value.y))
+
+Italy <- filter(merged2, Location == "ITA")
+
+ggplot(data = Italy) + geom_line(mapping = aes(x = Time, y = Value.x))
+ggplot(data = Italy) + geom_line(mapping = aes(x = Time, y = Value.y))
+
+
+
+
+cor.test(merged2$Value.x, merged2$Value.y)
